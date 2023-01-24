dae864a3924a7ae8c1246a7fe013fdc90cc60f89
huseyinerkal <>
2023-01-24 12:18:17 +0300

    added responsive

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,28 +1,9 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CustomButton from "../common/CustomButton";
 
-const HomeComponent = () => {
-  const [imgs, setImgs] = useState([]);
+const HomeComponent = ({ imgs }: any) => {
   const [columns, setColumns] = useState(8);
-
-  useEffect(() => {
-    fetch(
-      "https://pexelsdimasv1.p.rapidapi.com/v1/search?query=ocean&locale=en-US&per_page=50&page=1",
-      {
-        method: "GET",
-        headers: {
-          Authorization:
-            "zfYVk5YvwkjkfRskTnCUo8UEJfKf2olFC8lFGqGc1uVOT4YInbsqGdhh",
-
-          "X-RapidAPI-Key":
-            "8c7b64bd81mshbf8bd1ce84a3d21p137356jsn36e4da30c471",
-          "X-RapidAPI-Host": "PexelsdimasV1.p.rapidapi.com",
-        },
-      }
-    ).then((res) => res.json().then((res) => setImgs(res.photos)));
-  }, []);
-
   return (
     <section className="min-h-screen bg-black-200 text-white pt-14 flex items-center flex-col overflow-hidden">
       <div className="container flex flex-col m-4 items-center pt-16">
@@ -121,10 +102,10 @@ const HomeComponent = () => {
         </div>
       </div>
       <div
-        className={`grid pb-14 gap-5 px-8 w-screen`}
+        className={`grid pb-14 gap-1 px-2 md:px-8 w-screen`}
         style={{ gridTemplateColumns: `repeat(${columns},minmax(0,1fr))` }}
       >
-        {imgs?.map((item, index) => (
+        {imgs?.map((item: any, index: any) => (
           <div key={item?.id}>
             <div className={`relative aspect-h-1 aspect-w-1 `}>
               <Image
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,39 @@
 import HomeComponent from "@/components/Home";
 import Head from "next/head";
+import { FC } from "react";
 
-export default function Home() {
+type HomeProps = {
+  imgs: any;
+};
+
+const Home: FC<HomeProps> = ({ imgs }) => {
   return (
     <>
       <Head>
-        <title>Huso Art</title>
+        <title>Lexica</title>
       </Head>
-      <HomeComponent />
+      <HomeComponent imgs={imgs} />
     </>
   );
+};
+
+export async function getServerSideProps() {
+  const imgs = await fetch(
+    "https://pexelsdimasv1.p.rapidapi.com/v1/search?query=ocean&locale=en-US&per_page=50&page=1",
+    {
+      method: "GET",
+      headers: {
+        Authorization:
+          "zfYVk5YvwkjkfRskTnCUo8UEJfKf2olFC8lFGqGc1uVOT4YInbsqGdhh",
+
+        "X-RapidAPI-Key": "8c7b64bd81mshbf8bd1ce84a3d21p137356jsn36e4da30c471",
+        "X-RapidAPI-Host": "PexelsdimasV1.p.rapidapi.com",
+      },
+    }
+  ).then((res) => res.json().then((res) => res.photos));
+
+  // Pass data to the page via props
+  return { props: { imgs } };
 }
+
+export default Home;
